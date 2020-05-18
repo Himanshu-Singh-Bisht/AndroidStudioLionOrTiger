1fd28fefbd19581ce968cc3e9c32febbd0c29962
himanshu <bhimanshu031@gmail.com>
2020-05-18 19:18:31 +0530

    Second commit

--- app/src/main/java/com/himanshu/lionortiger/MainActivity.java
+++ app/src/main/java/com/himanshu/lionortiger/MainActivity.java
@@ -3,6 +3,8 @@ package com.himanshu.lionortiger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    // ADDING ON ONCLICK FUNCTION FOR IMAGE VIEWS
+    public void imageViewIsTapped(View imageView)
+    {
+        ImageView tappedImageView = (ImageView) imageView;
+
+        tappedImageView.setTranslationX(-2000);
+        tappedImageView.setImageResource(R.drawable.tiger);         // to set the image of  tiger.
+
+        tappedImageView.animate().translationXBy(2000f).alpha(1).rotation(3600).setDuration(1000);          // using 3 animation (for using alpha firstly set it to 0).
+
+
     }
 }
